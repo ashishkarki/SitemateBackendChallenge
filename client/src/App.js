dcba5da8144bf7d11b9b17dce31a73236b891503
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2024-09-05 21:31:48 +1000

    feat(client): add navigation links and improve hover effect

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,37 @@
-import './styles/index.css'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 
 import CreateIssue from './components/CreateIssue'
 import ReadIssues from './components/ReadIssues'
 import UpdateIssue from './components/UpdateIssue'
 import DeleteIssue from './components/DeleteIssue'
 
+import './styles/index.css'
+
 function App() {
   return (
     <div>
-      <h1>Issue Tracker</h1>
-      <CreateIssue />
-      <ReadIssues />
-      <UpdateIssue />
-      <DeleteIssue />
+      <Router>
+        <div className="container">
+          <h1>Issue Tracker</h1>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/create">Create Issue</Link></li>
+              <li><Link to="/read">View Issues</Link></li>
+              <li><Link to="/update">Update Issue</Link></li>
+              <li><Link to="/delete">Delete Issue</Link></li>
+            </ul>
+          </nav>
+          
+          <Routes>
+            <Route path="/" element={<h2>Welcome to Issue tracker!!</h2>} />
+            <Route path="/create" element={<CreateIssue />} />
+            <Route path="/read" element={<ReadIssues />} />
+            <Route path="/update" element={<UpdateIssue />} />
+            <Route path="/delete" element={<DeleteIssue />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   )
 }
--- server/src/index.js
+++ server/src/index.js
@@ -2,22 +2,31 @@ require('dotenv').config()
 
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
 const PORT = process.env.PORT || 8000
 
 const expressApp = express()
 
 // middlewares
+const cors = require('cors')
+expressApp.use(cors({
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}))
 expressApp.use(express.json())
 
-// Serve static files from the React app
-expressApp.use(express.static(path.join(__dirname, '../../client/build')))
+// Serve static files from the React app if the build folder exists in the client directory
+const buildPath = path.join(__dirname, '../../client/build')
+if(fs.existsSync(buildPath)) {
+  expressApp.use(express.static(buildPath))
+}
 
 // routes base path
 expressApp.use('/api/v1', require('./routes'))
 
 // Catch-all handler to serve React's index.html for any other routes
 expressApp.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/build', 'index.html'))
+  // res.sendFile(path.join(__dirname, '../../client/build', 'index.html'))
 })
 
 // start server
